e5451dd7d9dd604754a93c1553b2b1d21cdcfb81
George Radu <bluthundr3@gmail.com>
2023-03-29 11:03:03 +0300

    Added UserService and updated a few things

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Date;
 
 class Menu {
+    private Menu() {}
+    private static Menu menu = null;
+    private User activeUser;
+    private UserService userService;
+
+
+    public static Menu getInstance() {
+        if(menu == null)
+            menu = new Menu();
+        return menu;
+    }
+
+    public void setActiveUser(User user) {
+        this.activeUser = user;
+    }
+
+    public void run() {
+
+    }
+
     //TODO: Actiuni in meniu:
     // 1. Adauga User
     // 2. Login User
@@ -16,16 +39,71 @@ class Menu {
     // 12. Adauga notita in folder
     // 13. Tags
     // Maybe more to come
+}
 
+class Crypto {
+    private Crypto() {}
+    public static String encryptPassword(String password) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.update(password.getBytes());
+
+            byte[] resultByteArray = messageDigest.digest();
+            StringBuilder stringBuilder = new StringBuilder();
+            for(byte b : resultByteArray) {
+                stringBuilder.append(String.format("%02x", b));
+            }
+
+            return stringBuilder.toString();
+        }
+        catch(NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public static boolean compare(String hashPassword, String plainPassword) {
+        String resultHashPassword = Crypto.encryptPassword(plainPassword);
+        return hashPassword.equals(resultHashPassword);
+    }
+}
+
+class UserService {
+    User[] users;
+
+    public void addUser(String userName, String passwordHash) {
+        users = Arrays.copyOf(users, users.length + 1);
+        users[users.length - 1] = new User(userName, passwordHash);
+    }
+
+    public User getUserByUserName(String userName) {
+        for(User u : users)
+            if(userName.equals(u.getUserName()))
+                return u;
+        return null;
+    }
 }
 
 // Manages the authentication for user and protected notes that the user might access
 class AuthService {
+    private AuthService() {}
 
+    public static boolean loginUser(User user, String password) {
+        if(!Crypto.compare(user.getPasswordHash(), password))
+            return false;
+
+        Menu.getInstance().setActiveUser(user);
+        return true;
+    }
+
+    public static boolean accessProtectedNote(ProtectedGenericNote pNote, String password) {
+        return Crypto.compare(pNote.getPasswordHash(), password);
+    }
 }
 
 abstract class GenericFolder {
-    private GenericNote[] notes;
+    private String name;
 }
 
 class Trash extends GenericFolder{
@@ -52,12 +130,14 @@ abstract class GenericNote {
         dateModified = new Date();
         this.title = title;
     }
-
-
 }
 
 abstract class ProtectedGenericNote extends GenericNote{
+    String passwordHash;
 
+    public String getPasswordHash(){
+        return this.passwordHash;
+    }
 }
 
 class Note extends GenericNote{
@@ -82,11 +162,21 @@ class CheckListElement {
 
 class User {
     private String userName;
-    private String password;
+    private String passwordHash;
+
+    public User(String userName, String passwordHash) {
+        this.userName = userName;
+        this.passwordHash = passwordHash;
+    }
+
+    public String getUserName() { return this.userName; }
+
+    public String getPasswordHash() { return this.passwordHash; }
+
 }
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
